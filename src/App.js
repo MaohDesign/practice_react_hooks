85838150f18a444a970422c607521d8a946fe904
Mami Ohira <maoh.design@gmail.com>
2019-11-24 15:49:05 +0900

    Create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InputTextForm from "./component/atoms/Input";
+
+function App() {
+  return (
+    <div className="App">
+      <InputTextForm />
+    </div>
+  );
+}
+
+export default App;
